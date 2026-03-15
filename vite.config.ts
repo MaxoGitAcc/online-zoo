8ae97385d2eb41251d4546698ab9298ace80457c
MaxoGitAcc <maxowork19@gmail.com>
2026-03-15 17:38:21 +0400

    :fix: vite imges error

--- vite.config.ts
+++ vite.config.ts
@@ -20,21 +20,44 @@ export default defineConfig({
     {
       name: "copy-assets",
       closeBundle() {
+        // copy components
         mkdirSync("dist/assets/components", { recursive: true });
-        copyFileSync("assets/components/header.html", "dist/assets/components/header.html");
-        copyFileSync("assets/components/footer.html", "dist/assets/components/footer.html");
+        copyFileSync(
+          "assets/components/header.html",
+          "dist/assets/components/header.html",
+        );
+        copyFileSync(
+          "assets/components/footer.html",
+          "dist/assets/components/footer.html",
+        );
 
-        const imagesDirs = ["animaCards", "smalss"];
-        imagesDirs.forEach(dir => {
+        mkdirSync("dist/assets/icons", { recursive: true });
+        readdirSync("assets/icons").forEach((file) => {
+          copyFileSync(`assets/icons/${file}`, `dist/assets/icons/${file}`);
+        });
+
+        const imagesDirs = [
+          "animaCards",
+          "smalss",
+          "finalSection",
+          "payAndFeed",
+        ];
+        imagesDirs.forEach((dir) => {
           mkdirSync(`dist/assets/images/${dir}`, { recursive: true });
-          readdirSync(`assets/images/${dir}`).forEach(file => {
-            copyFileSync(`assets/images/${dir}/${file}`, `dist/assets/images/${dir}/${file}`);
+          readdirSync(`assets/images/${dir}`).forEach((file) => {
+            copyFileSync(
+              `assets/images/${dir}/${file}`,
+              `dist/assets/images/${dir}/${file}`,
+            );
           });
         });
 
-        mkdirSync("dist/assets/images", { recursive: true });
-        readdirSync("assets/images").forEach(file => {
-          if (file.endsWith(".png") || file.endsWith(".jpg") || file.endsWith(".webp")) {
+        readdirSync("assets/images").forEach((file) => {
+          if (
+            file.endsWith(".png") ||
+            file.endsWith(".jpg") ||
+            file.endsWith(".webp")
+          ) {
             copyFileSync(`assets/images/${file}`, `dist/assets/images/${file}`);
           }
         });
